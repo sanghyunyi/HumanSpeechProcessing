eb22a5c557d9a3c14393ab184da1dcbcdc35831b
sangyi92 <sangyi92@wafflestudio.com>
2019-03-03 00:56:20 -0800

    add decoding

--- src/fit.py
+++ src/fit.py
@@ -1,6 +1,7 @@
 from sklearn.linear_model import Ridge, RidgeCV, Lasso, LassoCV # Encoding
 from sklearn.svm import LinearSVC # Decoding
 from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
 import pickle as pkl
 import pandas as pd
 import numpy as np
@@ -31,55 +32,52 @@ def flat_to_3d(images, original_shape):
     images = images.reshape(original_shape[:-1])
     return images
 
-
 def cross_validation(reg, X, y, n_splits): #reg
-    corr_from_alpha = []
-    for alpha in ALPHAS:
-        reg.alpha = alpha
-        kf = KFold(n_splits=n_splits, shuffle=False)
-        corr_from_CV = []
-        for train_idx, test_idx in kf.split(X):
-            X_train, X_test = X.iloc[train_idx], X.iloc[test_idx]
-            y_train, y_test = y[train_idx], y[test_idx]
-            reg.fit(X_train, y_train)
-            prediction = reg.predict(X_test)
-            corr_list = []
-            for i in range(prediction.shape[-1]):
-                corr = np.corrcoef(prediction[:, i], y_test[:, i])[0, 1]
-                corr_list.append(corr)
-            corr_list = np.nan_to_num(np.array(corr_list))
-            corr_from_CV.append(corr_list)
-        corr_from_CV = np.average(corr_from_CV, axis=0)
-        corr_from_alpha.append(corr_from_CV)
-    corr_from_alpha = np.amax(corr_from_alpha, axis=0)
-    return corr_from_alpha
+    kf = KFold(n_splits=n_splits, shuffle=False)
+    corr_from_CV = []
+    for train_idx, test_idx in kf.split(X):
+        X_train, X_test = X.iloc[train_idx], X.iloc[test_idx]
+        y_train, y_test = y[train_idx], y[test_idx]
+        reg.fit(X_train, y_train)
+        prediction = reg.predict(X_test)
+        corr_list = []
+        for i in range(prediction.shape[-1]):
+            corr = np.corrcoef(prediction[:, i], y_test[:, i])[0, 1]
+            corr_list.append(corr)
+        corr_list = np.nan_to_num(np.array(corr_list))
+        corr_from_CV.append(corr_list)
+    corr_from_CV = np.average(corr_from_CV, axis=0)
+    return corr_from_CV
 
 
 def encoding(reg, features, images):
     X = features
     y, image_shape = flatten_brain(images)
-    #reg.fit(X, y)
-    #prediction = reg.predict(X)
-    #corr_list = []
-    #for i in range(prediction.shape[-1]):
-    #    corr = np.corrcoef(prediction[:, i], y[:, i])[0, 1]
-    #    corr_list.append(corr)
-    #corr_list = np.nan_to_num(np.array(corr_list))
-    corr_list = cross_validation(reg, X, y, 10)
-    corr_3d = flat_to_3d(corr_list, image_shape)
+    corr_from_alpha = []
+    for alpha in ALPHAS:
+        reg.alpha = alpha
+        corr_from_CV = cross_validation(reg, X, y, 10)
+        corr_from_alpha.append(corr_from_CV)
+    best_corr = np.amax(corr_from_alpha, axis=0)
+    corr_3d = flat_to_3d(best_corr, image_shape)
     return corr_3d
 
 def decoding(reg, features, images):
     X, image_shape = flatten_brain(images)
     y = features
-    reg.fit(X, y)
-
-    return None
+    score_from_alpha = []
+    for alpha in ALPHAS:
+        reg.C = 1./alpha
+        scores = cross_val_score(reg, X, y, cv=10, scoring='accuracy')
+        score = np.mean(scores)
+        score_from_alpha.append(score)
+    best_score = np.amax(score_from_alpha)
+    return best_score
 
 
 if __name__ == "__main__":
     brain_img = pkl.load(open('../data/brain.pkl', 'rb'))
-    feature = pd.read_pickle('../data/correct_feature.pkl').filter(regex='DA_com')
+    feature = pd.read_pickle('../data/correct_feature.pkl').filter(regex='POS')
     reg = Ridge()
     corr = encoding(reg, feature, brain_img)
     corr[corr < 0.2] = 0.
